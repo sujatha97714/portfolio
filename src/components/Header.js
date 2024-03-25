b42ce13dd711aa698ff80c5b5686c56aee3385b2
Sujatha Pushparaj (Student) <spushpar@sundevils.asu.edu>
2024-03-25 16:34:43 -0700

    Minor update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header id="header" className="alt">
-            <span class="logo"><img src="/assets/images/s.svg" alt="" height="100px"/></span>
+            <span className="logo"><img src="/assets/images/s.svg" alt="" height="100px"/></span>
             <h1>Sujatha Pushparaj</h1>
             <p> A programming enthusiast exploring the maze of web technologies. <br />
             MS Computer Engineering @ <a href="https://degrees.apps.asu.edu/masters-phd/major/ASU00/ESCENCMS/computer-engineering-computer-systems-ms?init=false&nopassive=true">Arizona State University</a></p>
